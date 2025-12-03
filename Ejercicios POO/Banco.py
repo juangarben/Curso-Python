95e1e3424f94fa6225f0a63d889556a601a2768e
Juan <juangarben34@gmail.com>
2025-12-04 00:15:47 +0100

    Banco - V1.0 Varios usuarios

--- Ejercicios POO/Banco.py
+++ Ejercicios POO/Banco.py
@@ -10,25 +10,26 @@ class Usuario:
         self.saldo=0
         self.logging=False
         
-    def Inicia_sesion(self):
+    def Inicia_sesion(self,nombre,password):
         
-        nombre_usuario_answ=input("Introduce el nombre de usuario: ")
-        password_answ=input("Introduce la clave: ")
-        if nombre_usuario_answ==self.__nombre and password_answ==self.__password:
+        if nombre==self.__nombre and password==self.__password:
             print("### Logging correcto!! ###")
             self.logging=True
+            return True
         else:
-            print("Error: Ususario o clave incorrectos!")
             self.logging=False
+            return False
             
             
-    def getAtributos(self):
-        print(f"\nNombre: {self.__nombre}\tPassword: {self.__password}\tCCC:{self.__num_cuenta}\tSaldo:{self.saldo}\n")
+    def __str__(self):
+        
+        return f"\nNombre: {self.__nombre}\tPassword: {self.__password}\tCCC:{self.__num_cuenta}\tSaldo:{self.saldo}\n"
             
 
 class Cajero_automatico:
     def __init__(self):
-        pass    
+        pass
+        
     
     def Hazte_cliente(self):
         nombre=input("Introduce un nombre usuario: ")
@@ -38,13 +39,13 @@ class Cajero_automatico:
         
     
     def Consulta_saldo(self,usuario):
-        
-        usuario.getAtributos()
+    
+        print(usuario)
             
     
     def Retirar_efectivo(self,usuario,cantidad):
         if usuario.saldo>=cantidad:
-            usuario.saldo-=cantidad
+           usuario.saldo-=cantidad
         else:
             print("Error: No hay saldo suficiente")
         
@@ -59,23 +60,41 @@ class Ejecutable:
     def __init__(self):
         
         opciones_menu=["Hazte cliente","Inicio Sesión","Consulta Saldo","Retirada efectivo","Ingreso efectivo"]
-        
-        
+        lista_usuarios=[]
         self.salir=False
+        self.logging=False
     
         while not self.salir:
             self.opcion_elegida=Menu("Cajero Automático",opciones_menu).crear_menu()
-            if self.opcion_elegida==1:
+            
+            if self.opcion_elegida==1: #Hazte cliente
                 
-                miUsuario=Cajero_automatico.Hazte_cliente(self)
+                lista_usuarios.append(Cajero_automatico.Hazte_cliente(self))
                 print("### Usuario registrado correctamente ###")
-                miUsuario.getAtributos()
+                print(lista_usuarios[-1])
                 input("Presiona Enter para continuar...")
 
-            elif self.opcion_elegida==2:
+            elif self.opcion_elegida==2: #Inicia sesión
                 try:
-                    miUsuario.Inicia_sesion()
+                    nombre_usuario_answ=input("Introduce el nombre de usuario: ")
+                    password_answ=input("Introduce la clave: ")
+                    contador=0
+                    
+                    for usuario in lista_usuarios:
+                        if usuario.Inicia_sesion(nombre_usuario_answ,password_answ):
+                            miUsuario=usuario
+                            self.logging=True
+                            break
+                        else:
+                            contador+=1
+                            self.logging=False
+                    
+                    if contador==len(lista_usuarios):
+                        print("Usuario o clave incorrectos")
+                        
                     input("Presiona Enter para continuar...")
+                    
+                    
                 except:
                     print("Error: No existe este usuario. Debes hacerte cliente primero")
                     input("Presiona Enter para continuar...")
@@ -84,7 +103,7 @@ class Ejecutable:
             elif self.opcion_elegida==3:
              
                 try:
-                    if miUsuario.logging:
+                    if self.logging:
                         Cajero_automatico.Consulta_saldo(self,miUsuario)
                         input("Presiona Enter para continuar...")
                     else:
@@ -98,7 +117,7 @@ class Ejecutable:
             elif self.opcion_elegida==4:
                 
                 try:
-                    if miUsuario.logging:
+                    if self.logging:
                         cantidad=int(input("Indica la cantidad de dinero que quieres retirar: "))
                         Cajero_automatico.Retirar_efectivo(self,miUsuario,cantidad)
                         print("### Retirada de efectivo realizada correctamente ###")
@@ -110,13 +129,12 @@ class Ejecutable:
                     print("Error: Para retirar efectivo debes loggearte primero")
                     input("Presiona Enter para continuar...")
                 
-                
-                
+                            
                 
             elif self.opcion_elegida==5:
                 
                 try:
-                    if miUsuario.logging:
+                    if self.logging:
                         cantidad=int(input("Indica la cantidad de dinero que quieres ingresar: "))
                         Cajero_automatico.Ingresar_efectivo(self,miUsuario,cantidad)
                         print("### Ingreso realizado correctamente!!! ###")
@@ -137,3 +155,10 @@ class Ejecutable:
 
 Ejecutable()
 
+""" Mejoras a implementar:
+    
+    1.- que ponga el nombre de usuario en el menu
+    2.- que se puedan hacer transferencias entre usuarios
+    3.- que te tengas que desloggear para volver a iniciar sesión
+    
+"""
--- Ejercicios POO/tempCodeRunnerFile.py
+++ Ejercicios POO/tempCodeRunnerFile.py
@@ -1,4 +1,12 @@
-except:
-                    print("Error: Para ingresar efectivo debes loggearte primero")
-                    input("Presiona Enter para continuar...")
-                
+elif self.opcion_elegida==3:
+             
+            #     try:
+            #         if #miUsuario.logging:
+            #             #Cajero_automatico.Consulta_saldo(self,miUsuario)
+            #             input("Presiona Enter para continuar...")
+            #         else:
+            #             print("Error: Para consultar el saldo debes loggearte primero")
+            #             input("Presiona Enter para continuar...")
+            #     except:
+            #         print("Error: Para consultar el saldo debes loggearte primero")
+            #         input("Presiona Enter para continuar...")
